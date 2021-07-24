f9ff07f350be7425e6b91d6dea1dcdc0ca6365ad
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-07-24 23:17:14 +0530

    (chore) Basic Setup

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ const mongoose = require("mongoose")
 dotenv.config()
 
 const PORT = process.env.PORT || 8000
+const MONGO_URI = process.env.MONGO_URI
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
-
-app.listen(PORT, () => {
-    console.log(`Running on http://localhost:${PORT}`)
+mongoose.connect(MONGO_URI, {useNewUrlParser:true, useUnifiedTopology: true} , (err) => {
+    if(err){
+        console.log(err)
+    } else {
+        app.listen(PORT, () => {
+            console.log(`Running on http://localhost:${PORT}`)
+        })
+    }
 })
